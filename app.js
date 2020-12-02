f82d3d790142f6512ac563b304ef17aadafe863c
Saikumar <saikumar9182@gmail.com>
2020-12-02 11:54:58 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,47 @@
+var btnTranslate= document.querySelector("#btn-translate");
+var txtInput= document.querySelector("#txt-input");
+var outputDiv= document.querySelector("#output");
+
+
+
+
+//btnTranslate.addEventListener("click", clickEventHandler())
+
+var serverURL= "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURL+"?"+"text=" + text
+}
+
+function errorHandler(error){
+    console.log("error occured",error)
+    alert("An error occured! Try later")
+}
+
+function clickEventHandler(){
+    var inputText= txtInput.value;
+    
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json =>
+        {
+          var translatedText =json.contents.translated;
+          outputDiv.innerText= translatedText;
+         
+        })
+
+    .catch(errorHandler)
+
+};
+
+btnTranslate.addEventListener("click", clickEventHandler)
+
+    
+
+
+
+
+
+
+
+
